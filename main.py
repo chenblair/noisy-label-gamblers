8a8b19f8b85986b7c5e9faa66d6f7229eb04d4cf
pliang279 <blairc@andrew.cmu.edu>
2020-04-17 04:27:04 -0400

    change code to make animal-10n work

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import torch.nn as nn
 import torch.nn.functional as F
 from torch.autograd import Variable
 import torchvision.transforms as transforms
+import torchvision.models as models
 from torchvision import datasets
 from data.mnist import MNIST
 from data.cifar import CIFAR10
@@ -17,6 +18,7 @@ import datetime
 import shutil
 import load_data
 import json
+import pdb
 
 def train(args,
           model,
@@ -26,7 +28,8 @@ def train(args,
           epoch,
           num_classes=10,
           use_gamblers=True,
-          text=False):
+          text=False,
+          save_info=''):
     model.train()
     loss_a = []
 
@@ -38,7 +41,7 @@ def train(args,
             if (data.size()[0] != args.batch_size):
                 continue
         else:
-            data, target, index = batch
+            data, target = batch[0], batch[1]
         data, target = data.to(device), target.to(device)
         optimizer.zero_grad()
         output = model(data)
@@ -49,6 +52,7 @@ def train(args,
             # if eps > num_classes, model learning is equivalent to nll
             eps = 1000 + num_classes
 
+            # python3 main.py --dataset animal-10n --lr 0.0001 --batch_size 128 --n_epoch 50 --lambda_type nll --seed 10 --gpu 1 --load_model
             # set lambda in the gambler's loss
             if (args.lambda_type == 'euc'):
                 eps = ((1 - output[:, num_classes])**2 + 1e-10) / (torch.sum(
@@ -65,14 +69,19 @@ def train(args,
             elif (args.lambda_type == 'gmblers'):
                 eps = args.eps
 
-            if (not use_gamblers):
-                eps = 1000 + num_classes
-
+        if (args.lambda_type == 'lq'):
+            q = 0.7
+            loss = -1 * F.nll_loss(output, target, reduction='none')
+            loss = (1 - (loss + 1e-10).pow(q)) / q
+            loss = torch.mean(loss)
         # compute gambler's loss
-        output = (output + (output[:, num_classes] / eps).unsqueeze(1) +
+        elif (use_gamblers and args.lambda_type != 'nll'):
+            output = (output + (output[:, num_classes] / eps).unsqueeze(1) +
                   1E-10).log()
-
-        loss = F.nll_loss(output, target)
+            loss = F.nll_loss(output, target)
+        else:
+            output = output.log()
+            loss = F.nll_loss(output, target)
         loss_a.append(loss.item())
         loss.backward()
         optimizer.step()
@@ -81,6 +90,9 @@ def train(args,
             print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}\t'.format(
                 epoch, batch_idx * len(data), len(train_loader.dataset),
                 100. * batch_idx / len(train_loader), loss.item()))
+    if (len(save_info) > 0):
+        print("saving info in {}".format(save_info))
+        json.dump(, open(save_info, 'w'))
     return np.mean(loss_a)
 
 
@@ -98,7 +110,7 @@ def test(args, model, device, test_loader, num_classes, text=False):
                 if (data.size()[0] != args.batch_size):
                     continue
             else:
-                data, target, index = batch
+                data, target = batch[0], batch[1]
 
             data, target = data.to(device), target.to(device)
             output = model(data)
@@ -130,7 +142,7 @@ def main():
         '--noise_rate',
         type=float,
         help='corruption rate, should be less than 1',
-        default=0.5)
+        default=0.0)
     parser.add_argument(
         '--noise_type',
         type=str,
@@ -146,7 +158,15 @@ def main():
         default=4,
         help='how many subprocesses to use for data loading')
     parser.add_argument('--epoch_decay_start', type=int, default=80)
-    parser.add_argument('--load_model', type=str, default="")
+    parser.add_argument(
+        '--load_model',
+        action='store_true',
+        default=False)
+    parser.add_argument(
+        '--use_scheduler',
+        action='store_true',
+        default=False)
+    parser.set_defaults(load_model=False)
 
     parser.add_argument(
         '--batch_size',
@@ -178,6 +198,8 @@ def main():
         '--lambda_type', type=str, help='[nll, euc, mid, exp, gmblers]', default="euc")
     parser.add_argument(
         '--start_gamblers', type=int, help='number of epochs before starting gamblers', default=0)
+    parser.add_argument(
+        '--gpu', type=int, help='gpu index', default=0)
 
     args = parser.parse_args()
 
@@ -254,8 +276,28 @@ def main():
         TEXT, vocab_size, word_embeddings, train_loader, valid_iter, test_loader = load_data.load_dataset(
             rate=args.noise_rate, batch_size=args.batch_size)
 
+    if args.dataset == 'animal-10n':
+        data_transform = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+        input_channel = 3
+        num_classes = 10
+        train_dataset = datasets.ImageFolder('data/animal-10n/train', transform=data_transform)
+        test_dataset = datasets.ImageFolder('data/animal-10n/test', transform=data_transform)
+        print('loading dataset...')
+        train_loader = torch.utils.data.DataLoader(
+            dataset=train_dataset,
+            batch_size=args.batch_size,
+            num_workers=args.num_workers,
+            drop_last=True,
+            shuffle=True)
+        test_loader = torch.utils.data.DataLoader(
+            dataset=test_dataset,
+            batch_size=args.batch_size,
+            num_workers=args.num_workers,
+            drop_last=True,
+            shuffle=False)
+    
     use_cuda = torch.cuda.is_available()
-    device = torch.device("cuda" if use_cuda else "cpu")
+    device = torch.device("cuda:{}".format(args.gpu) if use_cuda else "cpu")
     print("using {}".format(device))
 
     print('building model...')
@@ -269,23 +311,43 @@ def main():
         model = LSTMClassifier(args.batch_size, num_classes + 1, hidden_size,
                                vocab_size, embedding_length, word_embeddings).to(device)
         optimizer = LaProp(filter(lambda p: p.requires_grad, model.parameters()), lr=args.lr)
+    if args.dataset == 'animal-10n':
+        model = models.vgg19_bn(num_classes=num_classes + 1, dropout=0.2).to(device)
+        #optimizer = torch.optim.Adam(model.parameters(), lr=args.lr, weight_decay=0.0005)
+        optimizer = torch.optim.SGD(model.parameters(), momentum=0.9, lr=args.lr, weight_decay=0.0005, nesterov=True)
+        if (args.use_scheduler):
+            change_lr = lambda epoch: pow(0.2, int(epoch >= 50) + int(epoch >= 75))
+            scheduler = torch.optim.lr_scheduler.LambdaLR(optimizer, lr_lambda=change_lr)
 
     test_accs = []
     train_losses = []
     test_losses = []
     out = []
 
-    eee = 1 - args.noise_rate
-    criteria = (-1) * (eee * np.log(eee) + (1 - eee) * np.log(
-        (1 - eee) / (args.eps - 1)))
+    if args.early_stopping:
+        eee = 1 - args.noise_rate
+        criteria = (-1) * (eee * np.log(eee) + (1 - eee) * np.log(
+            (1 - eee) / (args.eps - 1)))
 
-    name = "{}_{}_{:.2f}_{}_{}".format(args.dataset, args.noise_type, args.noise_rate, args.eps, args.seed)
+    name = "{}_{}_{}_{:.2f}_{}_{}".format(args.dataset, args.lambda_type, args.noise_type, args.noise_rate, args.eps, args.seed)
+
+    if args.load_model:
+        checkpoint = torch.load("{}/{}.npy".format(args.result_dir, name))
+        model.load_state_dict(checkpoint['model_state_dict'])
+        optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
+        train_losses = checkpoint["train_loss"]
+        test_losses = checkpoint["test_loss"]
+        test_accs = checkpoint["test_acc"]
+        print("loaded {}".format(name))
+        print("{} epochs so far".format(len(train_losses)))
 
     if not os.path.exists(args.result_dir):
         os.system('mkdir -p %s' % args.result_dir)
 
     for epoch in range(1, args.n_epoch + 1):
         print(name)
+        for param_group in optimizer.param_groups:
+            print(epoch, param_group['lr'])
         train_loss = train(
             args,
             model,
@@ -294,28 +356,41 @@ def main():
             optimizer,
             epoch,
             num_classes=num_classes,
-            use_gamblers=(epoch >= args.start_gamblers),
+            use_gamblers=(epoch > args.start_gamblers),
             text=(args.dataset == 'imdb'))
         train_losses.append(train_loss)
         test_acc, test_loss = test(args, model, device, test_loader, num_classes, text=(args.dataset == 'imdb'))
         test_accs.append(test_acc)
         test_losses.append(test_loss)
         
-        # torch.save({
-        #     'model_state_dict': model.state_dict(),
-        #     'optimizer_state_dict': optimizer.state_dict(),
-        #     'loss': loss,
-        #     'test_acc': acc
-        # }, args.result_dir + "/" + name + "_model.npy")
+        if (str(train_loss) == 'nan'):
+            print('nan, breaking')
+            break
+        
+        if args.early_stopping and epoch >= args.start_gamblers and train_loss < criteria :
+            print('epoch: {}, loss fulfilled early stopping criteria: {} < {}'.format(epoch, train_loss, criteria))
+            break
+        
+        model_data = {
+            'command': " ".join(sys.argv),
+            'model_state_dict': model.state_dict(),
+            'optimizer_state_dict': optimizer.state_dict(),
+            "train_loss" : train_losses,
+            "test_loss" : test_losses,
+            "test_acc" : test_accs
+        }
+        if (args.use_scheduler):
+            scheduler.step()
+            model_data['scheduler_state_dict'] = scheduler.state_dict()
+        torch.save(model_data, "{}/{}.npy".format(args.result_dir, name))
 
     save_data = {
         "train_loss" : train_losses,
         "test_loss" : test_losses,
         "test_acc" : test_accs
     }
-    save_file = args.result_dir + "/" + name + ".json"
+    save_file = "{}/{}.json".format(args.result_dir, name)
     json.dump(save_data, open(save_file, 'w'))
-
 
 if __name__ == '__main__':
     main()
